f6c9dc264fbcf6c017502154c517e300e879e170
chenyu <977564830@qq.com>
2020-12-17 11:41:11 +0800

    定时任务脚本

--- bin.sh
+++ bin.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-# /bin/bash /home/yu/code/clockin/bin.sh > /home/yu/code/clockin/log/clockin.log 2>&1
+
+# 定时任务
+# 30 18 * * * /bin/bash /home/yu/code/clockin/bin.sh > /home/yu/code/clockin/log/clockin.log 2>&1
+# 50 8 * * * /bin/bash /home/yu/code/clockin/bin.sh > /home/yu/code/clockin/log/clockin.log 2>&1
 cd /home/yu/code/clockin && ./clockinbin pro
