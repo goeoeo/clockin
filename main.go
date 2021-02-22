48cb97167ee0a84a986ef3f517749de35e1ce6a9
chenyu <977564830@qq.com>
2021-02-22 16:54:02 +0800

    http服务更新

--- main.go
+++ main.go
@@ -2,22 +2,10 @@ package main
 
 import (
 	"github.com/phpdi/clockin/core"
-	"github.com/phpdi/clockin/httpserver"
-	"os"
 )
 
 //go build -o clockinbin main.go
 func main() {
-	httpserver.HttpServer()
-	//cmd()
-}
-
-func cmd() {
-
-	if len(os.Args) > 0 {
-		core.Run(os.Args[0])
-		return
-	}
-
-	core.Run("")
+	//httpserver.HttpServer()
+	core.Clockinbin()
 }
